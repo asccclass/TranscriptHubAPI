a02873e39112cb82d1f1bd3a38039b728646fb46
andyliu <justgps@gmail.com>
2025-07-26 11:28:07 +0800

    Initial Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# TranscriptHubAPI 
+主要用來取代 TranscriptHub 後端的 API 使用。
---
+++ router.go
@@ -0,0 +1,19 @@
+// router.go
+package main
+
+import(
+   "net/http"
+   "github.com/asccclass/sherryserver"
+)
+
+// Create your Router function
+func NewRouter(srv *SherryServer.Server, documentRoot string)(*http.ServeMux) {
+   router := http.NewServeMux()
+
+   // Static File server
+   staticfileserver := SherryServer.StaticFileServer{documentRoot, "index.html"}
+   staticfileserver.AddRouter(router)
+
+   return router
+}
+
